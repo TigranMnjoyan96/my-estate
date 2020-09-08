9f3560fc7068038fd2704e39ed15353c9547ce4b
TigranMnjoyan96 <mnjoyan1996@gmail.com>
2020-09-08 18:24:08 +0400

    added yandex map

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
+import YmapPlugin from 'vue-yandex-maps'
+
+
+
+Vue.use(ElementUI)
+Vue.use(YmapPlugin)
+
+
 
 Vue.config.productionTip = false
 
